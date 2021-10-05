215354434cc8deeed5822f1009ff00dbe53794a0
BeatrixDev <beatrix.ford@hotmail.com>
2021-10-05 10:35:52 -0300

    feat: add final code style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useCallback, useState } from "react";
+import { AppContainer, MainSection } from "./components/app.style";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./themes";
+import { Button } from "./components/button.style";
+
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const getOpositeTheme = useCallback(
+    () => (currentTheme === "light" ? "dark" : "light"),
+    [currentTheme]
+  );
+
   return (
-    <div className="App">
-      <main className="App-header"></main>
-    </div>
+    <ThemeProvider theme={themes[currentTheme]}>
+      <AppContainer>
+        <MainSection>
+          <h1>You are in {currentTheme} mode</h1>
+          <Button onClick={() => setCurrentTheme(getOpositeTheme())}>
+            switch to {getOpositeTheme()} mode
+          </Button>
+        </MainSection>
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes.js
@@ -0,0 +1,24 @@
+const lightTheme = {
+  color: "#39424E",
+  backgroundColor: "#D3D4E6",
+  button: {
+    textColor: "#39424E",
+    borderColor: "#151A69",
+    backgroundColor: "#151A69",
+  },
+};
+
+const darkTheme = {
+  color: "#D3D4E6",
+  backgroundColor: "#39424E",
+  button: {
+    textColor: "#D3D4E6",
+    borderColor: "#151A69",
+    backgroundColor: "#151A69",
+  },
+};
+
+export const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+};
